9e49a4135b0abded653220a67e3a56b4be57ed8a
caiyongliang <caiyongliang@phone580.com>
2022-10-08 16:44:23 +0800

    es

--- es/src/main/java/es/config/ElasticSearchConfig.java
+++ es/src/main/java/es/config/ElasticSearchConfig.java
@@ -1,5 +1,6 @@
 package es.config;
 
+import lombok.Data;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -8,19 +9,20 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
 /**
  * @author chase
  * @date 2022/9/26 5:30 PM
  */
-//@Data
-//@Configuration
-//@ConfigurationProperties(prefix = "elasticsearch")
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.elasticsearch.rest")
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
-    private String host;
-    private Integer port;
+    private String uris;
     private String password;
     private String username;
 
@@ -28,7 +30,7 @@ public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
     public RestHighLevelClient elasticsearchClient() {
         final CredentialsProvider provider = new BasicCredentialsProvider();
         provider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
-        RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost(host, port, "http"))
+        RestClientBuilder restClientBuilder = RestClient.builder(new HttpHost(uris, Integer.valueOf(uris.split(":")[1]), "http"))
                 .setHttpClientConfigCallback(f -> f.setDefaultCredentialsProvider(provider));
         return  new RestHighLevelClient(restClientBuilder);
     }
